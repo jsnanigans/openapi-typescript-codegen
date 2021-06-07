21ccfe7e0e9038899fc0b73a818cbc4aedffdf15
Brendan Mullins <brendan.mullins@9am.health>
2021-06-07 20:17:40 +0200

    Update dependencies

--- dist/index.js
+++ dist/index.js
@@ -2690,7 +2690,7 @@ var fetchGetRequestBody = {"compiler":[8,">= 4.3.0"],"main":function(container,d
 },"useData":true};
 
 var fetchGetResponseBody = {"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "async function getResponseBody(response: Response): Promise<any> {\n    try {\n        const contentType = response.headers.get('Content-Type');\n        if (contentType) {\n            const isJSON = contentType.toLowerCase().startsWith('application/json');\n            if (isJSON) {\n                return await response.json();\n            } else {\n                return await response.text();\n            }\n        }\n    } catch (error) {\n        console.error(error);\n    }\n    return null;\n}";
+    return "async function getResponseBody(response: Response): Promise<any> {\n    try {\n        const contentType = response.headers.get('Content-Type');\n        if (contentType) {\n            const isJSON = contentType.toLowerCase().startsWith('application/json');\n            if (isJSON) {\n                return await response.json();\n            } else {\n                return await response.blob();\n            }\n        }\n    } catch (error) {\n        console.error(error);\n    }\n    return null;\n}";
 },"useData":true};
 
 var fetchGetResponseHeader = {"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
